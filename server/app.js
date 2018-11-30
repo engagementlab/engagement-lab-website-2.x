7d594176bec484f15074bed2ba92f72ce5dc9a7f
Johnny Richardson <johnny.c.rich@gmail.com>
2018-11-30 13:18:52 -0500

    Removed passport/auth0 usage (broken out to bootstrapper).

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-'use strict';
+ 'use strict';
 /**
  * Engagement Lab Website v2.x
  * Developed by Engagement Lab, 2018
@@ -15,53 +15,10 @@ if(process.env.NODE_ENV !== 'test')
 	require('dotenv').load();
 
 const bootstrap = require('el-bootstrapper'),
-	  express = require('express');
-	  var passport = require('passport');
-	  var Auth0Strategy = require('passport-auth0');
+			express = require('express');
 
 
 var app = express();
-
-	// support json encoded bodies
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Enable view template compilation caching
-app.enable('view cache');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Configure Passport to use Auth0
-var strategy = new Auth0Strategy(
-	{
-	  domain: process.env.AUTH0_DOMAIN,
-	  clientID: process.env.AUTH0_CLIENT_ID,
-	  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-	  callbackURL:
-		process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-	},
-	function (accessToken, refreshToken, extraParams, profile, done) {
-	  // accessToken is the token to call Auth0 API (not needed in the most cases)
-	  // extraParams.id_token has the JSON Web Token
-	  // profile has all the information from the user
-	  return done(null, profile);
-	}
-  );
-
-  passport.use(strategy);
-  
-  // You can use this section to keep a smaller payload
-  passport.serializeUser(function (user, done) {
-	done(null, user);
-  });
-  
-  passport.deserializeUser(function (user, done) {
-	done(null, user);
-  });
-  
-
 bootstrap.start(
 	'./config.json', 
 	app,
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,5 @@
-/**
- * This file is where you define your application routes and controllers.
- *
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- *
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- *
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to hrouteren for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- *
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- *
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#router.VERB
- */
-
 const keystone = global.keystone;
 const express = require('express');
-const passport = require('passport');
 var router = express.Router();
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
@@ -35,29 +14,6 @@ var routes = {
     api: importRoutes('./api'),
     views: importRoutes('./views')
 };
-
-// keystone redirect
-// Perform the login, after login Auth0 will redirect to callback
-router.get('/keystone', passport.authenticate('auth0', {
-    scope: 'openid email profile'
-  }), function (req, res) {
-    res.redirect('/');
-  });
-
-  router.get('/callback', function (req, res, next) {
-    passport.authenticate('auth0', function (err, user, info) {
-      if (err) { return next(err); }
-      if (!user) { return res.redirect('/cms'); }
-      req.logIn(user, function (err) {
-        //   console.log('user',user)
-        if (err) { return next(err); }
-        const returnTo = req.session.returnTo;
-        delete req.session.returnTo;
-
-        res.redirect(returnTo || '/cms');
-      });
-    })(req, res, next);
-  });
 
 // Views
 router.get('/', routes.views.index);
@@ -79,8 +35,8 @@ router.get('/publications/:publication_key', routes.views.projects.publication);
 router.all('/projects/', routes.views.projects.projectsAll);
 router.get('/projects/:project_key', routes.views.projects.project);
 router.get('/projects/:subdirectory*?/:project_key', function(req, res, next) {
-	res.redirect('/projects/' + req.params.project_key);
-	next();
+  res.redirect('/projects/' + req.params.project_key);
+  next();
 });
 
 router.get('/cmap', routes.views.cmap);
@@ -96,18 +52,10 @@ router.all('/climatesmartboston', function(req, res, next) {
 });
 router.all('/api/cpi/register', keystone.middleware.api, routes.api.communityplanit.create);
 router.all('/api/tv/get', [keystone.middleware.api, keystone.middleware.cors], routes.api.tv.get);
-router.get('/authenticate', function (req, res, next) {
-    // const { _raw, _json, ...userProfile } = req.user;
-    console.log(req.session.passport.user)
-  });
 
 // Participatory Pokémon Go redirect
 router.all('/pokemon', function(req, res, next) {
     res.redirect('https://elab.us.launchpad6.com/');
 });
-
-// // Dynamic directory routes
-// router.get('/:directory', routes.views.directory);
-// router.get('/:directory/:subdirectory', routes.views.subdirectory);
 
 module.exports = router;
