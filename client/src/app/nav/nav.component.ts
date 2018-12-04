6e4ad82cdc86c66af11f5a2018629c1de8c812fb
Johnny Richardson <johnny.c.rich@gmail.com>
2018-12-03 19:57:39 -0500

    Footer and nav layout work.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TweenLite, TimelineLite } from "gsap";
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+	tl: TimelineLite; 
+
   constructor() { }
 
   ngOnInit() {
+
+
+  	this.tl = new TimelineLite({paused:true});
+    
+  	let tl = this.tl;
+  	let open = document.getElementById('open');
+    let close = document.getElementById('close');
+  	// let home = document.getElementById(ismobile.phone ? 'home-mobile' : 'home');
+  	let navEl = document.getElementById('menu');
+
+    TweenLite.set(open, {transformStyle:'preserve-3d'});
+    TweenLite.set(close, {transformStyle:'preserve-3d'});
+  	tl.fromTo(open, .1, {autoAlpha:1}, {autoAlpha:0, rotationY:90});
+    tl.fromTo(close, .2, {autoAlpha:0, rotationY:90}, {autoAlpha:1, rotationY:0, display:'block'}, '+=0.1');
+    tl.set(close, {css:{zIndex:21}});
+    tl.fromTo(navEl, .6, {autoAlpha:0}, {top:0, autoAlpha:1, ease:gsap.Quad.easeOut}, '+=0.1');
+
+  }
+
+  openNav() {
+
+  	document.getElementById('menu').classList.toggle('open');
+  	// this.tl.play();
+
   }
 
 }
