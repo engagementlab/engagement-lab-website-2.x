7333be22ef1b4762a182f9c275d650974689d22e
Johnny Richardson <johnny.c.rich@gmail.com>
2018-12-20 14:16:38 -0500

    Publications fixes.

--- client/src/app/publications/publication.component.ts
+++ client/src/app/publications/publication.component.ts
@@ -12,14 +12,32 @@ export class PublicationComponent implements OnInit {
 
   public iconWidth: number;
   public year: number;
+  public link: string;
+  public hasDownloads: boolean;
+  public hasResource: boolean;
+  public isExternal: boolean;
 
   constructor() {}
 
   ngOnInit() {
 
-      this.iconWidth = this.type === 'book' ? 37 : 50;
-      this.year = new Date(this.pub.date).getFullYear();
-      
-  }
+    let p = this.pub;
+
+    this.iconWidth = this.type === 'book' ? 37 : 50;
+    this.year = new Date(p.date).getFullYear();
+
+    this.hasDownloads = p.downloadUrls && p.downloadUrls.length > 0;
+    this.hasResource = p.articleResource && p.articleResource.file !== undefined;
+    this.isExternal = p.purchaseUrls && p.purchaseUrls.length > 0;
+
+    // Set link for pub
+    if(this.hasDownloads)
+      this.link = p.downloadUrls;
+    else if(this.hasResource)
+      this.link = p.articleResource.file.url;
+    else
+      this.link = p.purchaseUrls;
+
+    }
 
 }
