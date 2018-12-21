a21d0833a670860e72a498762ec9344da0646cae
Johnny Richardson <johnny.c.rich@gmail.com>
2018-12-21 14:45:40 -0500

    About page layout fixes.

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../utils/data.service';
+
+import * as ismobile from 'ismobilejs';
 
 @Component({
   selector: 'app-about',
@@ -8,13 +10,21 @@ import { DataService } from '../utils/data.service';
 })
 export class AboutComponent implements OnInit {
 
+  public isPhone: boolean;
+  public isTablet: boolean;
   public about: any;
   public partners: any[];
   public people: any[];
 
-  constructor(private _dataSvc: DataService) {  }
+  constructor(private _dataSvc: DataService) { 
+    
+    this.isPhone = ismobile.phone;
+    this.isTablet = ismobile.tablet;
+
+   }
 
   ngOnInit() {
+
 
     this._dataSvc.getDataForUrl('about/get/').subscribe(response => {
       this.about = response.about[0];    
