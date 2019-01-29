3d3281918b6fd06cea5f3e5e7b4258c31cbd6f73
Johnny Richardson <johnny.c.rich@gmail.com>
2019-01-29 15:19:01 -0500

    Include current cohort on people page.

--- server/routes/api/team.js
+++ server/routes/api/team.js
@@ -16,37 +16,80 @@ mongoose.Promise = require('bluebird');
 
 var buildData = (options, res) => {
 
-    let personFields = 'name title category key image.public_id url -_id';
+    let personFields = 'name title category key image.public_id url';
     let person = keystone.list('Person').model;
     let peopleData;
+    let queryProps = {};
+
+    var makeQuery = () => {
+
+        // Execute query    
+        Bluebird.props(queryProps)
+            .then(results => {
+                return res.status(200).json({
+                    status: 200,
+                    data: results
+                });
+            }).catch(err => {
+                console.error(err);
+            });
+
+    }
 
     if (options.id) {
+
         // Get person whose id specified
-        let addtlFields = 'bio email twitterURL fbURL linkedInURL githubURL websiteURL igURL';
+        let addtlFields = 'bio email twitterURL fbURL linkedInURL githubURL websiteURL igURL cohortYear -_id';
         peopleData = person.findOne({
             key: options.id
-        }, personFields + ' ' + addtlFields);
-    }
-    else {
-        // Get all people
-        peopleData = person.find({}, personFields)
-                            .sort([['sortOrder', 'ascending']]);
-    }
-
-    // Execute query    
-    Bluebird.props({
-            people: peopleData
-        })
-        .then(results => {
-            return res.status(200).json({
-                status: 200,
-                data: {
-                    people: results.people
-                }
-            });
-        }).catch(err => {
-            console.error(err);
+        }, personFields + ' ' + addtlFields)
+        .populate({
+                path: 'cohortYear',
+                select: 'name -_id'
         });
+        
+        queryProps = {
+            person: peopleData
+        };
+        makeQuery();
+
+    } else {
+        let filter = keystone.list('Filter').model;
+
+        // Get all people
+        // We have to get the current cohort year manually since mongoose can't join,
+        // and returning all CMAP people is inefficient
+        filter.findOne({
+            current: true,
+            category: 'Cohort'
+        }, '_id').exec((error, currentYr) => {
+
+            // Get faculty, staff, board
+            let peopleData = person.find({
+                    category: {
+                        $in: ['faculty leadership', 'staff', 'advisory board', 'faculty fellows']
+                    }
+                }, personFields + ' -_id')
+                .sort([
+                    ['sortOrder', 'ascending']
+                ]);
+
+            // Get current cohort
+            let studentData = person.find({
+                    cohortYear: currentYr,
+                    category: 'CMAP'
+                }, personFields + ' cohortYear -_id')
+                .sort([
+                    ['sortOrder', 'ascending']
+                ]);
+                
+            queryProps = {
+                staff: peopleData,
+                students: studentData
+            };
+            makeQuery();
+        });
+    }
 
 }
 
