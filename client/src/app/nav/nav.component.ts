037e2e8834d242a4e7b53de258f9f51452c9362f
Johnny Richardson <johnny.c.rich@gmail.com>
2020-02-26 15:17:05 -0500

    Re-introduced animations via gsap for menu, fix compiler errors for gsap, re-enable Ivy compiler.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,14 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component, AfterViewInit, ViewChild, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 
 import { DataService } from '../utils/data.service';
 import { filter } from 'rxjs/operators';
 
+import { TimelineLite, Circ } from "gsap/all";
+
 import { environment } from '../../environments/environment';
 import * as _ from 'underscore';
+import { isPlatformBrowser } from '@angular/common';
 
 interface Link {
   url: string,
@@ -30,20 +33,25 @@ export class NavComponent implements AfterViewInit {
   public searchResults: any[];
   public searchEnabled: boolean;
 
-  private btn: HTMLElement;
-  
   private wasLoading: boolean = false;
   private currentUrl: string;
 
+  private tl: TimelineLite;
+
+  @ViewChild('nav') nav: ElementRef;
   @ViewChild('searchField') searchField: ElementRef;
   @ViewChild('menuLinks') menuLinks: ElementRef;
   @ViewChild('home') homeLogo: ElementRef;
+  @ViewChild('menu') menu: ElementRef;
+  @ViewChild('menuBtn') menuBtn: ElementRef;
+  @ViewChild('menuOpen') menuBtnOpen: ElementRef;
+  @ViewChild('menuClose') menuBtnClose: ElementRef;
 
-  constructor(private _router: Router, private _dataSvc: DataService) {
+  constructor(@Inject(PLATFORM_ID) private platformId, private _router: Router, private _dataSvc: DataService) {
 
     // Get nav route when nav ends
-    _router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe(e => {
-      this.currentUrl = _router.url;
+    this._router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe(e => {
+      this.currentUrl = this._router.url;
 
       // Adjust logo size based on page
       let classes = this.homeLogo.nativeElement.classList;
@@ -67,14 +75,44 @@ export class NavComponent implements AfterViewInit {
 
   ngAfterViewInit() {
 
-  	// let menu = document.getElementById('menu');
-    // let show = document.querySelector('#menu-btn .close');
-    // let hide = document.querySelector('#menu-btn .open');
+    if (!isPlatformBrowser(this.platformId)) return;
 
+    this.tl = new TimelineLite({paused:true, reversed:true, onReverseComplete:() => {
+      this.menu.nativeElement.querySelectorAll('h3 a').forEach((el: HTMLElement) => {
+        el.classList.remove('visible');
+      });
+    }});
+    
+  	let tl = this.tl;
+
+    tl.add('start');
+    tl.set([this.nav.nativeElement, this.menuBtn.nativeElement], {className:'+=open'}, 'start');
+    
+    tl.fromTo(this.menuBtnClose.nativeElement, .7, {xPercent:100, autoAlpha:0}, { xPercent:0, autoAlpha:1, ease:Circ.easeOut});
+    tl.fromTo(this.menuBtnOpen.nativeElement, .7, {xPercent:0, autoAlpha:1}, { xPercent:100, autoAlpha:0, ease:Circ.easeOut}, '-=.7');
+
+    tl.fromTo(this.menu.nativeElement, .7, {autoAlpha:0}, {autoAlpha:1, display:'flex', ease:Circ.easeOut}, '-=.7');
+    tl.fromTo(document.getElementById('menu-overlay'), .5, {autoAlpha:0, display:'none'}, {autoAlpha:1, display:'block'}, '-=.7');
+
+    this.tl.staggerFromTo(this.menu.nativeElement.querySelectorAll('h3'), .2, {autoAlpha:0, yPercent:-20}, {autoAlpha:1, yPercent:0}, .1, '-=.5', () => {
+      this.menu.nativeElement.querySelectorAll('h3 a').forEach((el: HTMLElement) => {
+        el.classList.add('visible');
+      });
+    });
 
   }
 
   openCloseNav() {
+    
+    if(!this.tl.reversed()) {
+      this.tl.reverse().timeScale(1.3);
+      
+      this.searchField.nativeElement.value = '';
+      this.searchResults = null;
+    }
+    else
+      this.tl.play();
+    
   }
 
   // Is passed route active?
