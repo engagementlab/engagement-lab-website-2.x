e3a717164a7b009b53d35b39ea39d9db92fbea27
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-08 13:32:04 -0400

    refactor: **remove entire api dir as we move to usage of el-api**.

--- api/updates/0.0.4-search.js
+++
@@ -1,8 +0,0 @@
-const { keystone } = global;
-const Publication = keystone.list('Publication').model;
-
-module.exports = async (done) => {
-  await Publication.updateMany({ enabled: true }, { indexed: true });
-
-  done();
-};
