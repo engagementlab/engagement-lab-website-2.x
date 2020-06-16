5c0b41b07deb2f9a8320847ab32f9d270e7e03a2
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-16 18:06:44 -0400

    feat(client): update app component to display data service errors in app when they occur.

--- api/app.js
+++ api/app.js
@@ -11,12 +11,12 @@
  */
 
 const {
-  GraphQLScalarType,
+    GraphQLScalarType,
 } = require('graphql');
 const {
-  ApolloServer,
-  ApolloError,
-  gql,
+    ApolloServer,
+    ApolloError,
+    gql,
 } = require('apollo-server');
 const bootstrap = require('@engagementlab/el-bootstrapper');
 const express = require('express');
@@ -24,96 +24,96 @@ const winston = require('winston');
 const colors = require('colors');
 const elasticsearch = require('elasticsearch');
 
-const start = (productionMode) => {
-  const app = express();
+const start = productionMode => {
+    const app = express();
 
-  const logFormat = winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp(),
-    winston.format.align(),
-    winston.format.printf((info) => {
-      const {
-        timestamp, level, message, ...args
-      } = info;
+    const logFormat = winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf(info => {
+            const {
+                timestamp, level, message, ...args
+            } = info;
 
-      const ts = timestamp.slice(0, 19).replace('T', ' ');
-      return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
-    }),
-  );
+            const ts = timestamp.slice(0, 19).replace('T', ' ');
+            return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+        })
+    );
 
-  global.logger = winston.createLogger({
-    level: 'info',
-    format: logFormat,
-    transports: [
-      new winston.transports.Console(),
-    ],
-  });
-  global.elasti = undefined;
+    global.logger = winston.createLogger({
+        level: 'info',
+        format: logFormat,
+        transports: [
+            new winston.transports.Console()
+        ],
+    });
+    global.elasti = undefined;
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false, }));
 
-  app.use((req, res, next) => {
-    res.locals.db = global.keystone;
-    next();
-  });
+    app.use((req, res, next) => {
+        res.locals.db = global.keystone;
+        next();
+    });
 
-  apollo(app);
+    apollo(app);
 
-  if (process.env.SEARCH_ENABLED === 'true') searchBoot(app);
-  else boot(app, productionMode);
+    if (process.env.SEARCH_ENABLED === 'true') searchBoot(app);
+    else boot(app, productionMode);
 
-  return app;
+    return app;
 };
 
 const boot = (app, productionMode) => {
-  bootstrap.start(
-    './config.json',
-    app,
-    `${__dirname}/`, {
-      name: 'Engagement Lab Home CMS',
-    },
-    () => {
-      global.logger.info(colors.bgCyan.bold.black(`Content API started (${productionMode ? 'Production' : 'Development'} Mode).`));
-    },
-  );
+    bootstrap.start(
+        './config.json',
+        app,
+        `${__dirname}/`, {
+            name: 'Engagement Lab Home CMS',
+        },
+        () => {
+            global.logger.info(colors.bgCyan.bold.black(`Content API started (${productionMode ? 'Production' : 'Development'} Mode).`));
+        }
+    );
 };
 
-const searchBoot = (app) => {
-  global.elasti = new elasticsearch.Client({ node: process.env.ELASTISEARCH_NODE_URL });
-  global.elasti.ping((error) => {
-    if (error) {
-      global.logger.error('Elasticsearch ERROR!', error);
-    } else {
-      global.logger.info(colors.bgGray.yellow(`Search cluster running at ${process.env.ELASTISEARCH_NODE_URL}`));
-      boot(app);
-    }
-  });
+const searchBoot = app => {
+    global.elasti = new elasticsearch.Client({ node: process.env.ELASTISEARCH_NODE_URL, });
+    global.elasti.ping(error => {
+        if (error) {
+            global.logger.error('Elasticsearch ERROR!', error);
+        } else {
+            global.logger.info(colors.bgGray.yellow(`Search cluster running at ${process.env.ELASTISEARCH_NODE_URL}`));
+            boot(app);
+        }
+    });
 };
 
-const apollo = (app) => {
-  const schemaModules = require('./schemas')();
-  const schemas = schemaModules.map((mod) => mod.schema);
-  const queries = schemaModules.map((mod) => mod.queries);
-  const resolvers = schemaModules.map((mod) => mod.resolvers);
-  const resolversObj = {};
+const apollo = app => {
+    const schemaModules = require('./schemas')();
+    const schemas = schemaModules.map(mod => mod.schema);
+    const queries = schemaModules.map(mod => mod.queries);
+    const resolvers = schemaModules.map(mod => mod.resolvers);
+    const resolversObj = {};
 
-  /**
+    /**
    * Custom scalar type for Dates in schema
    */
-  const DateType = new GraphQLScalarType({
-    name: 'Date',
-    description: 'Date and time field.',
-    serialize(value) {
-      const result = new Date(value).toDateString();
-      return result;
-    },
-  });
+    const DateType = new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date and time field.',
+        serialize(value) {
+            const result = new Date(value).toDateString();
+            return result;
+        },
+    });
 
-  /**
+    /**
      * App's GraphQL types
      */
-  const TypeDefs = gql`
+    const TypeDefs = gql`
     scalar Date
     """Image type definition"""
     type Image {
@@ -132,38 +132,43 @@ const apollo = (app) => {
       html: String
       md: String
     }
+    """Name type definition"""
+    type Name {
+      first: String
+      last: String
+    }
     ${schemas.join(' ')}
 	  type Query {
       ${queries.join(' ')}
     }
   `;
-  // Assemble all resolver to object
-  resolvers.forEach((res) => Object.keys(res).forEach((k) => { resolversObj[k] = res[k]; }));
+    // Assemble all resolver to object
+    resolvers.forEach(res => Object.keys(res).forEach(k => { resolversObj[k] = res[k]; }));
 
-  /**
+    /**
      * App's GraphQL resolvers
      */
-  const Resolvers = {
-    Query: resolversObj,
-    Date: DateType,
-  };
+    const Resolvers = {
+        Query: resolversObj,
+        Date: DateType,
+    };
 
-  /**
+    /**
      * App's Apollo server instance
      */
-  const Apollo = new ApolloServer({
-    typeDefs: TypeDefs,
-    resolvers: Resolvers,
-    formatError: (err) => {
-      // Otherwise return the original error.  The error can also
-      // be manipulated in other ways, so long as it's returned.
-      global.logger.error(err);
-      return err;
-    },
-  });
+    const Apollo = new ApolloServer({
+        typeDefs: TypeDefs,
+        resolvers: Resolvers,
+        formatError: err => {
+            // Otherwise return the original error.  The error can also
+            // be manipulated in other ways, so long as it's returned.
+            global.logger.error(err);
+            return err;
+        },
+    });
 
-  // Mount apollo middleware (/graphql)
-  app.use(Apollo.getMiddleware());
+    // Mount apollo middleware (/graphql)
+    app.use(Apollo.getMiddleware());
 };
 
 module.exports = start;
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { Title } from '@angular/platform-browser';
-import { trigger, transition, style, query, group, animate } from '@angular/animations';
+import {
+    trigger,
+    transition,
+    style,
+    query,
+    group,
+    animate
+} from '@angular/animations';
 
 import { environment } from '../environments/environment';
 import { DataService } from './utils/data.service';
@@ -9,21 +16,32 @@ import { DataService } from './utils/data.service';
 export const projectTransition = [
     query('#top', [
         style({ transform: 'translate3d(77%, 0, 0)' }),
-        animate(0, style({ transform: 'translate3d(0, 0, 0)' })),
-    ]),
+        animate(0, style({ transform: 'translate3d(0, 0, 0)' }))
+    ])
 ];
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    animations: [trigger('routerAnimations', [transition('project => project', projectTransition)])],
+    animations: [
+        trigger('routerAnimations', [
+            transition('project => project', projectTransition)
+        ])
+    ]
 })
 export class AppComponent implements OnInit {
     public isQABuild: boolean;
+
+    public dataErrors: string;
+
     title = 'Engagement Lab @ Emerson College';
 
-    constructor(private _router: Router, private _titleSvc: Title, private _dataSvc: DataService) {
+    constructor(
+        private _router: Router,
+        private _titleSvc: Title,
+        private _dataSvc: DataService
+    ) {
         this.isQABuild = environment.qa;
         this._titleSvc.setTitle((this.isQABuild ? '(QA) ' : '') + this.title);
     }
@@ -34,7 +52,9 @@ export class AppComponent implements OnInit {
 
             // Reset relevant initiative page elements by default
             document.getElementById('logo-img').classList.remove('white');
-            if (!evt.url.includes('initiatives')) document.getElementById('initiative-bg').classList.value = '';
+            if (!evt.url.includes('initiatives')) {
+                document.getElementById('initiative-bg').classList.value = '';
+            }
 
             if (evt.url.indexOf('/#') === 0) return;
 
@@ -43,11 +63,15 @@ export class AppComponent implements OnInit {
         });
 
         // TODO: Watch for any data/graphql errors
-        this._dataSvc.errors.subscribe(value => {});
+        this._dataSvc.errors.subscribe(value => {
+            if (value) {
+                this.dataErrors = value.join(', ');
+            }
+        });
     }
 
     public prepareRouteTransition(outlet: any): void {
-        const animation = outlet.activatedRouteData['animation'] || {};
-        return animation['value'] || null;
+        const animation = outlet.activatedRouteData.animation || {};
+        return animation.value || null;
     }
 }
