fbe5b6db12b301a9846f272dbd682eaa5332f904
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-24 14:19:52 -0400

    fix(client): working scully output via new routes plugin.

--- client/plugins/keys.js
+++ client/plugins/keys.js
@@ -1,34 +1,46 @@
 const axios = require('axios');
 const { registerPlugin } = require('@scullyio/scully');
 
-const queryData = async (type) => {
-  const body = `query { all${type}Pages { key } }`;
-  const response = await axios.post('http://localhost:3000/graphql', { query: body });
+const queryData = async (queryName) => {
+    const body = `query { ${queryName} { key } }`;
+    const response = await axios.post('http://localhost:3000/graphql', { query: body });
 
-  return response.data.data;
+    return response.data.data;
 };
 
 const initiativeIdPlugin = async (route, config) => {
-  // Obtain all initiative keys via graphql query
-  const response = await queryData('Initiative');
-  const routes = [];
+    // Obtain all initiative keys via graphql query
+    const response = await queryData('allInitiativePages');
+    const routes = [];
 
-  response.allInitiatives.forEach((res) => {
-    routes.push({ route: `/initiative/${res.key}` });
-  });
-  return Promise.resolve(routes);
+    response.allInitiativePages.forEach((res) => {
+        routes.push({ route: `/initiative/${res.key}` });
+    });
+    return Promise.resolve(routes);
 };
 const projectIdPlugin = async (route, config) => {
-  // Obtain all project keys via graphql query
-  const response = await queryData('Project');
-  const routes = [];
+    // Obtain all project keys via graphql query
+    const response = await queryData('allProjectPages');
+    const routes = [];
 
-  response.allProjects.forEach((res) => {
-    routes.push({ route: `/project/${res.key}` });
-  });
-  return Promise.resolve(routes);
+    response.allProjectPages.forEach((res) => {
+        routes.push({ route: `/project/${res.key}` });
+    });
+    return Promise.resolve(routes);
 };
+const eventIdPlugin = async (route, config) => {
+    // Obtain all event keys via graphql query
+    const response = await queryData('allEvents');
+    const routes = [];
+
+    response.allEvents.forEach((res) => {
+        routes.push({ route: `/event/${res.key}` });
+    });
+    return Promise.resolve(routes);
+};
+
 
 const validator = async (config) => [];
 registerPlugin('router', 'initiatives', initiativeIdPlugin, validator);
 registerPlugin('router', 'projects', projectIdPlugin, validator);
+registerPlugin('router', 'events', eventIdPlugin, validator);
