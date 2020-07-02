7a2ac3f0b82afd0a00fd6274db3920e091188be9
Johnny Richardson <johnny.c.rich@gmail.com>
2020-07-02 16:13:31 -0400

    ci: git checkout before DB export commit.

--- db.sh
+++ db.sh
@@ -22,6 +22,7 @@ for collection in $Collections; do
     mongoexport --uri $DB -c $collection -o ./bin/db/$collection.json
 done
 
+git checkout .
 git remote set-url origin git@github.com:engagementlab/engagement-lab-website-2.x.git
 git add bin/db
 git commit --message "Automated database export (Travis build #$TRAVIS_BUILD_NUMBER)"
