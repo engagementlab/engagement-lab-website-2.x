9f11d73c2d93f61bfa00c20c229155f3a3fea5dd
Johnny Richardson <johnny.c.rich@gmail.com>
2020-07-22 15:05:32 -0400

    feat(api): added Research model/schema.

---
+++ api/models/Research.js
@@ -0,0 +1,54 @@
+/**
+ * Engagement Lab Website v2.x
+ *
+ * Research Landing page Model
+ * @module Research
+ * @class Research
+ * @author Johnny Richardson
+ *
+ * For field docs: http://keystonejs.com/docs/database/
+ *
+ * ==========
+ */
+
+const { keystone, } = global;
+const { Types, } = keystone.Field;
+/**
+ * Research model
+ * @constructor
+ * See: http://keystonejs.com/docs/database/#lists-options
+ */
+const Research = new keystone.List('Research', {
+    label: 'Research Page',
+    singular: 'Research Page',
+    plural: 'Research Page',
+    nodelete: true,
+    nocreate: true,
+    autokey: {
+        path: 'key',
+        from: 'name',
+        unique: false,
+    },
+});
+
+/**
+ * Model Fields
+ * @main Research
+ */
+Research.add({
+    name: {
+        type: String, default: 'Research Page', hidden: true, required: true, initial: true, noedit: true,
+    },
+    blurb: {
+        type: Types.Textarea,
+        required: true,
+        initial: true,
+        note: 'This is the blurb that shows on `/research/projects/`',
+    },
+});
+
+/**
+ * Model Registration
+ */
+Research.defaultSort = '-createdAt';
+Research.register();
