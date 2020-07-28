3173e716e1a48bfa2173dbd4d090c1d9e7c1e058
Johnny Richardson <johnny.c.rich@gmail.com>
2020-07-28 13:38:44 -0400

    fix(client): fixed events not showing on index.

--- client/src/app/events/index.component.ts
+++ client/src/app/events/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as _ from 'underscore';
 import { DataService } from '../utils/data.service';
+import { keyframes } from '@angular/animations';
 
 @Component({
     selector: 'app-index',
@@ -12,7 +13,7 @@ export class EventIndexComponent implements OnInit {
 
     public pastEvents: any;
 
-    constructor(private _dataSvc: DataService) { }
+    constructor(private _dataSvc: DataService) {}
 
     async ngOnInit() {
         // const response = await this._dataSvc.getSet('events');
@@ -26,13 +27,14 @@ export class EventIndexComponent implements OnInit {
             }
         `;
         const response = await this._dataSvc.getSet('events', query);
-        this.groupDates(response);
+        this.groupDates(response['allEvents']);
     }
 
-    groupDates(data: any): void {
-        const allEvents = { data };
-        _.each(allEvents, (event) => {
-            event.future = new Date(event.date).getTime() > new Date().getTime();
+    groupDates(allEvents: object): void {
+        Object.keys(allEvents).forEach(key => {
+            let event = allEvents[key];
+            event.future =
+                new Date(event.date).getTime() > new Date().getTime();
         });
 
         this.events = _.where(allEvents, { future: true });
