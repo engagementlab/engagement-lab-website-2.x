ede2522dde5e420e9768be7e70e15aef116b12ad
Johnny Richardson <johnny.c.rich@gmail.com>
2020-08-06 18:36:45 -0400

    feat(api): Studio model/schema, Resource model fix.

---
+++ api/models/Studio.js
@@ -0,0 +1,239 @@
+/**
+ * Engagement Lab Website v2.x
+ *
+ * Studio Model
+ * @module studio
+ * @author Johnny Richardson
+ *
+ * For field docs: http://keystonejs.com/docs/database/
+ *
+ * ==========
+ */
+
+const { keystone, } = global;
+const { Types, } = keystone.Field;
+
+/**
+ * @module studio
+ * @constructor
+ * See: http://keystonejs.com/docs/database/#lists-options
+ */
+const Studio = new keystone.List('Studio', {
+    autokey: {
+        path: 'key',
+        from: 'name',
+        unique: true,
+    },
+});
+
+/**
+ * Model Fields
+ * @main Studio
+ */
+Studio.add(
+    'Metadata', {
+        name: {
+            type: String,
+            label: 'Name',
+            required: true,
+            initial: true,
+            index: true,
+        },
+
+        enabled: {
+            type: Types.Boolean,
+            label: 'Enabled',
+            default: true,
+            note: 'Determines if this studio appears on the live site.',
+        },
+        status: {
+            type: Types.Select,
+            options: 'Completed, Ongoing',
+            required: true,
+            initial: true,
+        },
+        customUrl: {
+            type: String,
+            label: 'Custom URL',
+            note: 'Must be format of "studio-url". Overrides default "/studios/studio/studio-name".',
+        },
+    },
+
+    'Studio Information', {
+
+        semester: {
+            type: String,
+            required: true,
+            initial: true,
+            note: 'e.g. _Spring 2020_',
+        },
+
+        faculty: {
+            type: Types.Relationship,
+            required: true,
+            initial: true,
+            ref: 'Person',
+            filters: {
+                type: ['faculty leadership', 'faculty fellows'],
+            },
+            many: true,
+        },
+        department: {
+            type: String,
+            required: true,
+            initial: true,
+            note: 'e.g. _Visual and Media Arts_',
+        },
+        sponsor: {
+            type: String,
+        },
+        students: {
+            type: Types.TextArray,
+            required: true,
+            initial: true,
+        },
+        relatedLinks: {
+            type: Types.Relationship,
+            ref: 'Resource',
+            many: true,
+        },
+        collaborators: {
+            type: Types.TextArray,
+        },
+        contact: {
+            type: String,
+        },
+        introduction: {
+            type: Types.Markdown,
+            required: true,
+            initial: true,
+        },
+        impact: {
+            type: Types.Markdown,
+            required: true,
+            initial: true,
+            label: 'Social Impact',
+        },
+        roles: {
+            type: Types.Markdown,
+            required: true,
+            initial: true,
+            label: 'Role of Technology + Media',
+        },
+
+    },
+
+    'Studio Media', {
+        instructions: {
+            type: String,
+            label: 'Please read!',
+            noedit: true,
+            note: '**All images below need to be very high quality.** <br />' +
+         '_Background Image_: Image shown as studio background. <br />' +
+         '_Gallery Images_: Images below main studio info. To re-order, remove and upload again. <br />' +
+         '_Image Captions_: Please specify in order of images. If an image has no caption, enter **#** in text field.',
+
+        },
+        // Image for studio BG
+        bgImage: {
+            type: Types.CloudinaryImage,
+            label: 'Background Image',
+            folder: 'homepage-2.0/studios',
+            autoCleanup: true,
+        },
+        // Images for studio gallery
+        galleryImages: {
+            type: Types.CloudinaryImages,
+            folder: 'homepage-2.0/studios',
+            autoCleanup: true,
+        },
+        galleryImageCaptions: {
+            type: Types.TextArray,
+        },
+
+    }
+);
+
+/**
+ * Methods
+ * =============
+ */
+
+// Remove a given resource from all studios that referenced it (videos and articles as of now)
+Studio.schema.statics.removeResourceRef = (resourceId, callback) => {
+    Studio.model.update({
+        $or: [{
+            videos: resourceId,
+        }, {
+            articles: resourceId,
+        }, {
+            blogs: resourceId,
+        }, {
+            files: resourceId,
+        }],
+    },
+
+    {
+        $pull: {
+            videos: resourceId,
+            articles: resourceId,
+            blogs: resourceId,
+            files: resourceId,
+        },
+    },
+
+    {
+        multi: true,
+    },
+
+    (err, result) => {
+        callback(err, result);
+
+        if (err) console.error(err);
+    });
+};
+
+/**
+ * Hooks
+ * =============
+ */
+Studio.schema.pre('save', next => {
+    // Save state for post hook
+    this.wasNew = this.isNew;
+
+    // Override key w/ custom URL if defined
+    if (this.customUrl && this.customUrl.length > 0) { this.key = this.customUrl; }
+
+    next();
+});
+
+Studio.schema.post('save', (doc, next) => {
+    // Make a post to slack when this Studio is updated
+    // keystone.get('slack').Post(Studio.model, this, true);
+
+    if (process.env.SEARCH_ENABLED === 'true') {
+    // Index doc on elasticsearch
+        global.elasti.index({
+            index: 'listing',
+            type: 'studio',
+            id: doc._id.toString(),
+            body: {
+                name: doc.name,
+                key: doc.key,
+                content: doc.byline,
+                description: doc.description,
+            },
+        }, (err, resp, status) => {
+            console.log(resp, status);
+            if (err) console.error(err);
+        });
+    }
+    next();
+});
+
+/**
+ * Model Registration
+ */
+Studio.defaultSort = 'sortOrder';
+Studio.defaultColumns = 'name, enabled, featured';
+Studio.register();
---
+++ api/schemas/Studio.js
@@ -0,0 +1,62 @@
+/**
+ * Engagement Lab Website v2.x
+ *
+ * Studios schema
+ * @module studios
+ * @class studios
+ * @author Johnny Richardson
+ *
+ * ==========
+ */
+const Studios = {
+
+    schema: `
+    type StudioIntro {
+        summary: String
+    }
+    type Studio {
+      id: ID!
+      key: String
+      name: String!
+      customUrl: String
+      enabled: Boolean
+      status: String
+      semester: String
+      faculty: [Person]
+      department: String
+      sponsor: String
+      students: [String]
+      relatedLinks: [Resource]
+      collaborator: [String]
+      contact: String
+      introduction: Markdown!
+      impact: Markdown!
+      roles: Markdown!
+      bgImage: Image
+      galleryImages: [Image]
+      galleryImageCaptions: [String]
+    }
+  `,
+    queries: ['allStudios: [Studio], getStudio(key: String): Studio, studiosIntro: StudioIntro'],
+    resolvers: {
+        allStudios: async () => global.keystone.list('Studio').model.find({ enabled: true, }).exec(),
+
+        getStudio: async (parent, args) => {
+            const res = await global.keystone.list('Studio').model.findOne({ $or: [{ key: args.key, }, { customUrl: args.key, }], })
+                .populate({
+                    path: 'faculty',
+                    select: 'key name -_id',
+                })
+                .populate({
+                    path: 'relatedLinks',
+                    select: 'name file url',
+                })
+                .exec();
+            // return GetAdjacent(project);
+            return res;
+        },
+        studiosIntro: async () => global.keystone.list('StudiosIntro').model.findOne({}).exec(),
+    },
+
+};
+module.exports = Studios;
