27f133643389266cc683927e4b29394c0d1eb22a
Johnny Richardson <johnny.c.rich@gmail.com>
2020-08-10 18:43:10 -0400

    feat(client): WIP studios/grad component.

--- client/src/app/studios/graduate/graduate.component.ts
+++ client/src/app/studios/graduate/graduate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/utils/data.service';
 
 @Component({
     selector: 'app-graduate',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./graduate.component.scss'],
 })
 export class StudiosGraduateComponent implements OnInit {
-    constructor() {}
+    public content: any;
+    public partners: any[];
 
-    ngOnInit(): void {}
+    constructor(private dataSvc: DataService) {}
+
+    async ngOnInit(): Promise<void> {
+        const query = `
+          {
+            studiosIntro {
+                gradSummary
+                gradSummaryImage {
+                    public_id
+                }
+            }
+          }
+      `;
+        const response = await this.dataSvc.getSetWithKey(
+            'studios',
+            'graduate',
+            query,
+        );
+        this.content = response['studiosIntro'];
+    }
 }
