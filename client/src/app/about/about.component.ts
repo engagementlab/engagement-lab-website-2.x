f06d3543cfc4091efe4bab5f8da394f8bba4ed80
Johnny Richardson <johnny.c.rich@gmail.com>
2020-08-19 18:46:00 -0400

    feat(client): About page WIP.

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -4,18 +4,11 @@ import { DataService } from '../utils/data.service';
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
-    styleUrls: ['./about.component.scss']
+    styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-    public isPhone: boolean;
-
-    public isTablet: boolean;
-
-    public about: any;
-
+    public content: any;
     public partners: any[];
-
-    public people: any[];
 
     constructor(private _dataSvc: DataService) {}
 
@@ -23,18 +16,12 @@ export class AboutComponent implements OnInit {
         const query = `
             {
                 allAboutPages {
-                    missionStatement 
                     summary1
                     summary2
-                    images
+                    image
                     {
                         public_id 
                     }
-                    research
-                    workshops
-                    tools
-                    teaching
-                    design
                 }
                 allPartners {
                     name 
@@ -43,21 +30,10 @@ export class AboutComponent implements OnInit {
                     }
                     url
                 }
-                allStaffPeople {
-                    name {
-                        first
-                        last
-                    }
-                    key
-                    image {
-                        public_id
-                    }
-                }
             }
         `;
         const response = await this._dataSvc.getSet('about', query);
-        this.about = response['allAboutPages'];
+        this.content = response['allAboutPages'];
         this.partners = response['allPartners'];
-        this.people = response['allStaffPeople'];
     }
 }
