6d738e141159a747b99d75b80a3f0ba960a5dab3
Johnny Richardson <johnny.c.rich@gmail.com>
2020-09-09 17:51:25 -0400

    fix: studios intro, newsletter endpoint.

--- api/routes/index.js
+++ api/routes/index.js
@@ -36,7 +36,7 @@ const Routes = () => {
     });
 
     router.post('/post/contact', routes.get.contact.send);
-    router.post('/post/newsletter/:email', routes.get.newsletter.signup);
+    router.post('/api/post/newsletter/:email', routes.get.newsletter.signup);
 
     // Error
     router.get('*', (req, res) => {
