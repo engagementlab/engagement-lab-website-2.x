62e00ae6e16d76ab60347635014a51933d67484f
johnnycrich <johnny.c.rich@gmail.com>
2020-11-18 16:56:33 -0500

    build: scully plugin fix.

--- client/plugins/routes.js
+++ client/plugins/routes.js
@@ -19,6 +19,9 @@ const createPa11yConfig = (routes) => {
   });
 
   let config = `{
+        "defaults": {
+          "hideElements": "#qa-build, #qa-border"
+        },
         "urls":  [${allRoutes}]
     }`;
 
