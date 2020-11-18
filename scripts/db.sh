d3f75d41a0082f796fdac3d0bf46594fe51c36ea
johnnycrich <johnny.c.rich@gmail.com>
2020-11-18 14:26:38 -0500

    build: no longer make commit only of DB diff in CI build.

--- scripts/db.sh
+++ scripts/db.sh
@@ -26,17 +26,17 @@ for collection in $Collections; do
     fi
 done
 
-git config --global user.name "ELDevBot"
-git config --global user.email engagementlab@emerson.edu
+# git config --global user.name "ELDevBot"
+# git config --global user.email engagementlab@emerson.edu
 
-git branch temp
-git checkout temp
+# git branch temp
+# git checkout temp
 
-git remote set-url origin git@github.com:engagementlab/engagement-lab-website-2.x.git
-git add bin/db
-git commit --message "Automated database export (Travis build #$TRAVIS_BUILD_NUMBER)"
-git branch -f master temp
-git checkout master
+# git remote set-url origin git@github.com:engagementlab/engagement-lab-website-2.x.git
+# git add bin/db
+# git commit --message "Automated database export (Travis build #$TRAVIS_BUILD_NUMBER) [ci skip]"
+# git branch -f master temp
+# git checkout master
 
-git branch -d temp
-git push origin master
+# git branch -d temp
+# git push origin master
