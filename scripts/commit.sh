bbe61892f002e04f847ee2ebc6b9e4fd532fec2f
Johnny Richardson <johnny.c.rich@gmail.com>
2020-12-10 17:38:37 -0500

    refactor(build): changed several build scripts to work under GH action.

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -8,9 +8,9 @@ git config --global user.email engagementlab@emerson.edu
 git branch temp
 git checkout temp
 
-git remote set-url origin git@github.com:engagementlab/engagement-lab-website-2.x.git
+git remote set-url origin https://$GH_TOKEN@github.com/engagementlab/engagement-lab-website-2.x.git
 git add bin/*
-git commit --message "Automated database export and generated static app (Travis build #$TRAVIS_BUILD_NUMBER) [ci skip]"
+git commit --message "Automated database export and generated static app (GH action #$GHA_RUNID) [ci skip]"
 git branch -f master temp
 git checkout master
 
--- scripts/db.sh
+++ scripts/db.sh
@@ -25,18 +25,3 @@ for collection in $Collections; do
         echo "Skip sessions"
     fi
 done
-
-# git config --global user.name "ELDevBot"
-# git config --global user.email engagementlab@emerson.edu
-
-# git branch temp
-# git checkout temp
-
-# git remote set-url origin git@github.com:engagementlab/engagement-lab-website-2.x.git
-# git add bin/db
-# git commit --message "Automated database export (Travis build #$TRAVIS_BUILD_NUMBER) [ci skip]"
-# git branch -f master temp
-# git checkout master
-
-# git branch -d temp
-# git push origin master
