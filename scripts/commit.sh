bf36d341fc0f8d023f90ed8530effdb3047fd372
Johnny Richardson <johnny.c.rich@gmail.com>
2020-12-14 14:22:32 -0500

    fix(build): GH actions and auto-commit script fix.

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,14 +5,8 @@ echo "Commit staging database and generated static app."
 git config --global user.name "ELDevBot"
 git config --global user.email engagementlab@emerson.edu
 
-git branch temp
-git checkout temp
-
 git remote set-url origin https://$GH_TOKEN@github.com/engagementlab/engagement-lab-website-2.x.git
 git add bin/*
-git commit --message "Automated database export and generated static app (GH action #$GHA_RUNID) [ci skip]"
-git branch -f master temp
-git checkout master
+git commit --message "Automated database export and generated static app (GH action #$GHA_RUNID) [skip ci]"
 
-git branch -d temp
-git push origin master
+git push
