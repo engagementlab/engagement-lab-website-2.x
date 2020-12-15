8c1590036d2e7ed0b03eab662063fbc78ccd6a58
Johnny Richardson <johnny.c.rich@gmail.com>
2020-12-15 17:52:14 -0500

    feat(client): a11y and performance improvements.

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -7,6 +7,6 @@ git config --global user.email engagementlab@emerson.edu
 
 git remote set-url origin https://$GH_TOKEN@github.com/engagementlab/engagement-lab-website-2.x.git
 git add bin/*
-git commit --message "Automated database export and generated static app (GH action #$GHA_RUNID) [skip ci]"
+git commit --message "Automated database export and generated static app (GH action #$GHA_RUNNUM) [skip ci]"
 
 git push
