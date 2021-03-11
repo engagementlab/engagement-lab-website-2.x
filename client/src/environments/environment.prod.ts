386e5e1cb7343df664ffc843bcd09575c4aca2e7
Johnny Richardson <johnny.c.rich@gmail.com>
2021-03-11 14:18:10 -0500

    refactor(client): updated api_url for prod.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     searchEnabled: false,
     universal: true,
     data_url: 'http://localhost:3000',
-    api_url: 'https://api.elab.emerson.edu/home',
+    api_url: 'https://api.elab.emerson.edu/engagement-lab-home',
 };
