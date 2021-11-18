f2b694f8a59e25e6e778d902d3ce1d327693316a
Johnny Richardson <johnny.c.rich@gmail.com>
2021-11-18 12:25:18 -0500

    build(client): scully config fixes.

--- client/scully.default.config.ts
+++ client/scully.default.config.ts
@@ -7,6 +7,7 @@ exports.config = {
     projectRoot: './src',
     projectName: 'elab-web',
     outDir: '../bin/app',
+    distFolder: './dist/browser',
     routes: {
         // All studios pages
         '/studios/studio/:key': {
