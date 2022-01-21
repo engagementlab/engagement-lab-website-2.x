3dc36cc1ffe6cffcbd05ca2e3909d03b4a6049b2
Johnny Richardson <johnny.c.rich@gmail.com>
2022-01-21 17:38:38 -0500

    feat(client): working POC for new studios filter.

--- client/src/app/studios/index/index.component.ts
+++ client/src/app/studios/index/index.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 import { DataService } from 'src/app/utils/data.service';
+
+import * as _ from 'underscore';
 
 @Component({
     selector: 'app-studios-index',
     templateUrl: './index.component.html',
     styleUrls: ['./index.component.scss'],
+
+    animations: [
+        trigger('inOutAnimation', [
+            transition(':enter', [
+                style({ transform: 'scale(0)' }),
+                animate(
+                    '.2s cubic-bezier(0.190, 1.000, 0.220, 1.000)',
+                    style({ transform: 'scale(1)', opacity: 1 }),
+                ),
+            ]),
+            transition(':leave', [
+                style({ transform: 'scale(1)' }),
+                animate(
+                    '.2s cubic-bezier(0.190, 1.000, 0.220, 1.000)',
+                    style({ transform: 'scale(0)', opacity: 0 }),
+                ),
+            ]),
+        ]),
+    ],
 })
 export class StudiosIndexComponent implements OnInit {
     public content: any;
     public partners: any[];
+
+    public currentStudio: any;
+    public currentSelectorKey: string;
 
     constructor(private dataSvc: DataService) {}
 
@@ -27,6 +53,25 @@ export class StudiosIndexComponent implements OnInit {
                     public_id
                   }
             }
+
+            allStudios {
+              key
+              name
+              thumb { 
+                  public_id
+              }
+            }
+
+            allStudioInitiatives {
+              key
+              name
+              studios {
+                key
+              }
+              thumb { 
+                  public_id
+              }
+            }
           }
       `;
         const response = await this.dataSvc.getSetWithKey(
@@ -34,6 +79,25 @@ export class StudiosIndexComponent implements OnInit {
             'intro',
             query,
         );
-        this.content = response['studiosIntro'];
+        this.content = response;
+    }
+
+    public applySelector(selector: string) {
+        this.currentSelectorKey = null;
+        if (!selector) {
+            this.currentStudio = null;
+            return;
+        }
+
+        this.currentStudio = _.findWhere(this.content.allStudioInitiatives, {
+            key: selector,
+        });
+    }
+
+    public isSelected(selector: string) {
+        return (
+            !this.currentStudio ||
+            _.pluck(this.currentStudio.studios, 'key').indexOf(selector) > -1
+        );
     }
 }
