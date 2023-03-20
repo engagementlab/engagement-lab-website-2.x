960b353a720e721c0e1668f832a538d1257ac403
Johnny Richardson <johnny.c.rich@gmail.com>
2023-03-20 17:45:37 -0400

    fix new API path.

--- client/src/environments/environment.qa.ts
+++ client/src/environments/environment.qa.ts
@@ -6,5 +6,5 @@ export const environment = {
     universal: true,
     api_url: 'http://localhost:3000',
     data_url: 'https://qa.elab.emerson.edu',
-    new_api_url: 'https://qa.transformnarratives.org/cms',
+    new_api_url: 'https://cms.elab.emerson.edu/tngvi/rest/news',
 };
