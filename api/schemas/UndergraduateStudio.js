2e818656e6d6c07637655b419c883de4924476f9
Johnny Richardson <johnny.c.rich@gmail.com>
2023-03-23 15:41:54 -0400

    feat(api): UndergraduateStudio schema/model updates, Image type change.

--- api/schemas/UndergraduateStudio.js
+++ api/schemas/UndergraduateStudio.js
@@ -9,17 +9,19 @@
  * ==========
  */
 const UndergraduateStudio = {
-
     schema: `
     type UndergraduateStudio {
       id: ID!
       name: String!
+      enabled: Boolean
       requiredCourse: Boolean
       description: Markdown
       faculty: [Person]
       semester: String!
       url: String
       year: AcademicYear
+      video: String
+      videoThumbnail: Image
     }
     type AcademicYear {
       label: String!
@@ -27,11 +29,13 @@ const UndergraduateStudio = {
   `,
     queries: ['allUndergraduateStudios: [UndergraduateStudio]', 'allYears: [AcademicYear]'],
     resolvers: {
-        allUndergraduateStudios: async () => global.keystone.list('UndergraduateStudio').model.find()
-            .populate('faculty').populate('year').exec(),
+        allUndergraduateStudios: async () => global.keystone
+            .list('UndergraduateStudio')
+            .model.find({ enabled: true, })
+            .populate('faculty')
+            .populate('year')
+            .exec(),
         allYears: async () => global.keystone.list('AcademicYear').model.find({}, 'label').sort({ label: 'desc', }).exec(),
-
     },
-
 };
 module.exports = UndergraduateStudio;
