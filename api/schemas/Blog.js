c67724059f9ef27c384bc5ad79734cb5fc205aee
Johnny Richardson <johnny.c.rich@gmail.com>
2023-04-05 13:54:26 -0400

    fix(api): optimize archive blog item images.

--- api/schemas/Blog.js
+++ api/schemas/Blog.js
@@ -24,6 +24,9 @@ const Blog = {
         allBlogItems: async () => global.keystone.list('Blog').model.find({ enabled: true, }).sort({ datePosted: -1, }).exec(),
         getBlogItem: async (parent, args) => {
             const newsItem = await global.keystone.list('Blog').model.findOne({ key: args.key, }).exec();
+            // Optimize image tags
+            newsItem.body.html = newsItem.body.html.replace(/upload\/v1/gm, 'upload/c_scale,f_auto,w_700/v1/');
+            newsItem.body.html = newsItem.body.html.replace('<img src', '<img style="max-width:700px" src');
             return newsItem;
         },
     },
